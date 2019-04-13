4f27451c1bac3461d73a812e9006dc95be09f0c2
Amigooo <xizhouh@gmail.com>
2019-04-13 17:22:21 +0800

    init

---
+++ packages/module-a/lib/module-a.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const core = require('@xizhouh/lerna-quick-start-core');
+
+module.exports = moduleA;
+
+function moduleA() {
+    core();
+}
---
+++ packages/module-b/lib/module-b.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const core = require('@xizhouh/lerna-quick-start-core');
+
+module.exports = moduleB;
+
+function moduleB() {
+    core();
+}
